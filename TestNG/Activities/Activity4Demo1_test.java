06fe1fa627e01db260a9f774db80a66f323f0067
Prajwalgowda1989 <142589317+Prajwalgowda1989@users.noreply.github.com>
2023-09-06 16:03:08 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4Demo1_test.java
@@ -0,0 +1,15 @@
+package Activities;
+import org.testng.annotations.Test;
+
+//testing4.xml has been created for the execution of the program
+public class Activity4Demo1_test {
+    @Test
+    public void firstTestCase() {
+        System.out.println("I'm in first test case from demoOne Class");
+    }
+
+    @Test
+    public void secondTestCase() {
+        System.out.println("I'm in second test case from demoOne Class");
+    }
+}
---
+++ TestNG/Activities/Activity4Demo2_test.java
@@ -0,0 +1,10 @@
+package Activities;
+import org.testng.annotations.Test;
+
+//testing4.xml has been created for the execution of the program
+public class Activity4Demo2_test {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
